21913512be6b01bc9629d7bbd9ac4d351e89223a
wataru yamazaki <16939675+wxyamazaki@users.noreply.github.com>
2019-08-04 19:37:27 +0900

    プロジェクトを追加

---
+++ src/main/java/com/google/cloud/vision/client/GoogleCloudVisionClientApplication.java
@@ -0,0 +1,13 @@
+package com.google.cloud.vision.client;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoogleCloudVisionClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GoogleCloudVisionClientApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/google/cloud/vision/client/GoogleCloudVisionClientApplicationTests.java
@@ -0,0 +1,16 @@
+package com.google.cloud.vision.client;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class GoogleCloudVisionClientApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
