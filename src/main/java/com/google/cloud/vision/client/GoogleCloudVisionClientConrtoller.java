0b6798577a8400b9064c26ecdf3b1b7de70f9875
wataru yamazaki <16939675+wxyamazaki@users.noreply.github.com>
2019-08-04 22:36:34 +0900

    ローカルに配置した画像を、ローカルのホストOSからリクエスト

--- src/main/java/com/google/cloud/vision/client/GoogleCloudVisionClientConrtoller.java
+++ src/main/java/com/google/cloud/vision/client/GoogleCloudVisionClientConrtoller.java
@@ -1,16 +1,60 @@
 package com.google.cloud.vision.client;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.ResourceLoader;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.google.cloud.vision.v1.AnnotateImageRequest;
+import com.google.cloud.vision.v1.AnnotateImageResponse;
+import com.google.cloud.vision.v1.BatchAnnotateImagesResponse;
+import com.google.cloud.vision.v1.Feature;
+import com.google.cloud.vision.v1.Feature.Type;
+import com.google.cloud.vision.v1.Image;
+import com.google.cloud.vision.v1.ImageAnnotatorClient;
+import com.google.protobuf.ByteString;
+
 @RestController
 public class GoogleCloudVisionClientConrtoller {
 
-  @GetMapping(path = "/google/cloud/vision/client", produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
-  public ResponseEntity<?> get() {
+  @Autowired
+  private ResourceLoader resourceLoader;
 
-    return ResponseEntity.ok("hello world.");
+  @GetMapping(path = "/google/cloud/vision/client", produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
+  public ResponseEntity<?> get() throws IOException {
+
+    // Instantiates a client
+    try (ImageAnnotatorClient vision = ImageAnnotatorClient.create()) {
+
+      // Reads the image file into memory
+      Path path = Paths.get(resourceLoader.getResource("classpath:image/original.jpg").getURI());
+      byte[] data = Files.readAllBytes(path);
+      ByteString imgBytes = ByteString.copyFrom(data);
+
+      // Builds the image annotation request
+      List<AnnotateImageRequest> requests = new ArrayList<>();
+      Image img = Image.newBuilder().setContent(imgBytes).build();
+      Feature feat = Feature.newBuilder().setType(Type.SAFE_SEARCH_DETECTION).build();
+      AnnotateImageRequest request = AnnotateImageRequest.newBuilder()
+          .addFeatures(feat)
+          .setImage(img)
+          .build();
+      requests.add(request);
+
+      // Performs label detection on the image file
+      BatchAnnotateImagesResponse response = vision.batchAnnotateImages(requests);
+      List<AnnotateImageResponse> responses = response.getResponsesList();
+
+      return ResponseEntity.ok(responses.toString());
+    }
   }
 }
